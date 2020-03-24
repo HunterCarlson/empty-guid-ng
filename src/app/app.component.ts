47a8250b6e43efc64d132b9dec20b366a33b99a0
Hunter Carlson <carlson.hunter@gmail.com>
2020-03-24 16:28:47 -0700

    initial page including syntax highlighting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewChecked } from '@angular/core';
+
+import { HighlightService } from './services/highlight.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'empty-guid-ng';
+export class AppComponent implements AfterViewChecked {
+
+  emptyGuid = '00000000-0000-0000-0000-000000000000';
+  code = `CAST(CAST(0 AS BINARY) AS UNIQUEIDENTIFIER)`;
+
+  highlighted = false;
+
+  constructor(private highlightService: HighlightService) {}
+
+  ngAfterViewChecked() {
+    if (this.code && !this.highlighted) {
+      this.highlightService.highlightAll();
+      this.highlighted = true;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HighlightService } from './services/highlight.service';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    HighlightService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
