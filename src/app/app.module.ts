753cc6347a8b3b003778b73e8d25926489c0d34b
Hunter Carlson <carlson.hunter@gmail.com>
2020-04-12 19:54:41 -0700

    added copy to clipboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ClipboardModule } from '@angular/cdk/clipboard'
 
 import { AppComponent } from './app.component';
 import { HighlightService } from './services/highlight.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ClipboardModule
   ],
   providers: [
     HighlightService
